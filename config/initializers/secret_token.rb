3e31b5ae44360904e269456937ab955ff9792f44
krishnau2 <krishnau2@gmail.com>
2013-04-11 16:18:01 +0530

    Initail commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BttSurvey::Application.config.secret_token = 'd927b04539c22eef596f113a2e9354792710dc01ed043676641a13b3242d6ec07e4f47732ede553c292a6ff94db184bfebc5110f76bcc17b560173a321d270ea'
