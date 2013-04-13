43ce5d3d7de9ee1bfe62afcf74afb8e4482de11b
krishnau2 <krishnau2@gmail.com>
2013-04-13 19:55:42 +0530

    table for questions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130411110615) do
+ActiveRecord::Schema.define(:version => 20130411121635) do
+
+  create_table "questions", :force => true do |t|
+    t.integer  "survey_id"
+    t.text     "content"
+    t.string   "type"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "surveys", :force => true do |t|
     t.string   "name"
