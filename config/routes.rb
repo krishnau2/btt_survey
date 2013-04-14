9876d3020fca4bc2516cfe520c7587b26f7c19fe
krishnau2 <krishnau2@gmail.com>
2013-04-14 13:25:57 +0530

    changed root route to home#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 BttSurvey::Application.routes.draw do
+  get "home/index"
+
   resources :surveys
 
 
@@ -51,7 +53,7 @@ BttSurvey::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
