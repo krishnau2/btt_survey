435122fbfb2e37dc75595fe6d015dc8400eb70bc
krishnau2 <krishnau2@gmail.com>
2013-04-15 16:30:38 +0530

    Migrations and schema for survey responses and its answers

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,27 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130413162140) do
+ActiveRecord::Schema.define(:version => 20130414122200) do
+
+  create_table "answers", :force => true do |t|
+    t.integer  "response_id"
+    t.integer  "question_id"
+    t.text     "content"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "questions", :force => true do |t|
     t.integer  "survey_id"
     t.text     "content"
     t.string   "type"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "responses", :force => true do |t|
+    t.integer  "survey_id"
+    t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
